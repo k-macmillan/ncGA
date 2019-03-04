d6d0cc03551699015d1c564ea7ea242423a34e80
Kyle MacMillan <macattackftw@gmail.com>
2019-03-04 15:14:38 -0700

    Updated main loop for our new strategy.

--- ga.py
+++ ga.py
@@ -2,28 +2,21 @@ import numpy as np
 
 
 class GA():
-    def __init__(self, pool=100, generations=1000, circles=50):
-        self.pool_size = pool
-        self.pool = [0] * self.pool_size
-        self.max_gens = generations
-        self.generation = 0
+    def __init__(self, population=100, generations=1000, circles=50):
+        self.pop_size = population
+        self.pop = [0] * self.pop_size
+        self.gens = generations
         self.circles = circles
         # self.genome = np.dtype([('center', f7),('radius', f7), ('intensity', np.int8), ('alpha', np.int8) ])
-        self.InitializePool()
-        self.Run()
 
-    def InitializePool(self):
-        """Initialize pool"""
-        print('Initalizing...')
-        pool = 0
-        while pool != self.pool_size:
-            self.MakeIndividual(pool)
-            pool += 1
+    def Reset(self):
+        self.generation = 0
+        self.img_fitness = 0
 
-    def MakeIndividual(self, i):
-        # individual = np.zeros((self.circles,), dtype=self.genome)
-        for _ in range(self.circles):
-            self.FillGenomes()
+    def InitializePop(self):
+        """Initialize pop"""
+        for i in range(self.pop_size):
+            self.pop[i] = self.FillGenomes()
 
     def FillGenomes(self):
         """Generates a genome"""
@@ -34,28 +27,40 @@ class GA():
         alpha = 100
         return np.array([center, radius, intensity, alpha])
 
-    def Run(self):
+    def Run(self, image):
         """Run the Genetic Algorithm"""
+        self.LoadImage(image)
+        self.Reset()
         print('Running...')
-        while self.generation != self.max_gens:
-            self.Evaluate()     # 1
-            if self.generation % 50 == 0:
-                self.Draw()
-
-            self.Breed()        # 2
-            self.Mutate()       # 3
-            self.generation += 1
-        self.Evaluate()
+        self.epoch = 0
+        while self.epoch != self.circles:
+            self.Draw()
+            self.img_fitness = self.EvaluateImage()
+            self.InitializePop()
+            for _ in range(self.gens):
+                self.Breed()        # 2
+                self.Mutate()       # 3
+            self.UpdateImage()
+            self.epoch += 1
+        self.EvaluatePop()
+        self.UpdateImage()
         self.Draw()
 
+    def LoadImage(self, image):
+        """Will load an image file into the format we need"""
+        self.image = 0
+        self.art = 0
 
 # 1
-    def Evaluate(self):
+    def EvaluateImage(self):
+        """Evaluates the current epoch image against self.image"""
+        return 0
+
+    def EvaluatePop(self):
         """Evaluates each individual and sorts them"""
-        for i in range(self.pool_size):
-            self.Fitness(self.pool[i])
+        for i in range(self.pop_size):
+            self.Fitness(self.pop[i])
         self.SortPopulation()
-        pass
 
     def Fitness(self, individual):
         """Scores fitness for an individual"""
@@ -68,8 +73,10 @@ class GA():
 # 2
     def Breed(self):
         """Selection and Crossover to generate a new population"""
+        # Breeding will likely increase number of generations needed but will 
+        # yield a better solution.
         new_pop_size = 0
-        while new_pop_size != self.pool_size:
+        while new_pop_size != self.pop_size:
             a, b = self.Selection()
             self.Crossover(a, b)
             new_pop_size += 1
@@ -85,16 +92,21 @@ class GA():
 
 # 3
     def Mutate(self):
-        """Mutatation applied to pool"""
+        """Mutatation applied to pop"""
         pass
 
 
+# 4 
+    def UpdateImage(self):
+        """Update self.art with the most fit individual"""
+        pass
 
     def Draw(self):
         """Draw the output to the screen"""
-        print('Drawing generation: ', self.generation)
+        print('Drawing epoch: ', self.epoch)
 
 
 
 if __name__ == '__main__':
     ga = GA()
+    ga.Run("imagepath")
