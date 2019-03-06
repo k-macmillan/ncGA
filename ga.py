4d581cf7dbe9f7ce1f585da05c054303c4622368
Kyle MacMillan <macattackftw@gmail.com>
2019-03-05 19:56:34 -0700

    Got breeding set up. Crossover was in work but hit a snag with numpy and custom dtypes.

--- ga.py
+++ ga.py
@@ -4,22 +4,16 @@ import matplotlib.pyplot as plt
 
 
 class GA():
+    ELITISM = 0.10
     def __init__(self, population=100, generations=100, circles=50):
         self.pop_size = population
-        self.pop = [0] * self.pop_size
         self.gens = generations
         self.circles = circles
         self.center = np.dtype([('x', np.uint16), ('y', np.uint16)])
-        self.genome = np.dtype([('center', self.center), ('radius', np.float64), ('intensity', np.uint8) ])
-        # Examples:
-        # c = np.array((9, 3), dtype=self.center)
-        # g = np.array((c, 2.3, 200), dtype=self.genome)
-        # print('radius: ', g['radius'])
-        # print('x:      ', g['center']['x'])
-        # exit()
+        self.genome = np.dtype([('center',self.center), ('radius', np.float64), ('intensity', np.uint8) ])
+        self.pop = np.zeros((self.pop_size, ), dtype=self.genome)
 
     def Reset(self):
-        self.generation = 0
         self.img_fitness = 0
 
     def Run(self, image):
@@ -34,7 +28,11 @@ class GA():
             self.InitializePop()    # 1
             for _ in range(self.gens):
                 self.EvaluatePop()
+                # print(self.pop)
                 self.Breed()        # 2
+                # print()
+                # print(self.pop)
+                # exit()
                 self.Mutate()       # 3
             self.EvaluatePop()            
             self.UpdateImage()
@@ -70,14 +68,12 @@ class GA():
             self.max_radius = self.max_dim
         else:
             self.max_radius = (self.img_fitness / self.max_image ) * self.max_dim
-        print('max rad: ', self.max_radius)
-        # exit()
-
 # 1
     def InitializePop(self):
         """Initialize pop"""
         self.DefineMaxCircleRadius()
         for i in range(self.pop_size):
+            temp = self.FillGenomes()
             self.pop[i] = self.FillGenomes()
 
     def FillGenomes(self):
@@ -85,15 +81,10 @@ class GA():
         center = np.array((np.random.random_integers(self.width) - 1,
                            np.random.random_integers(self.height) - 1),
                            dtype=self.center)
-        # radius = np.random.random_sample() * self.max_radius
-        # print(self.max_radius)
-        # exit()
-        # radius = self.max_radius
-        # print(self.max_radius / 12.0)
-        # exit()
         radius = np.random.uniform(low=max(1.0, self.max_radius / 12.0), high=self.max_radius / 2.0)
         intensity = np.random.random_integers(256) - 1
-        return np.array((center, radius, intensity), dtype=self.genome)
+        ret_val = np.array((center, radius, intensity), dtype=self.genome)
+        return ret_val
 
     def EvaluatePop(self):
         """Evaluates each individual and sorts them"""
@@ -128,22 +119,60 @@ class GA():
 # 2
     def Breed(self):
         """Selection and Crossover to generate a new population"""
-        # Breeding will likely increase number of generations needed but will 
-        # yield a better solution.
+        new_pop = np.zeros((self.pop_size, ), dtype=self.genome)
 
-        new_pop_size = 0
+        # Take the most fit and keep them
+        royalty = int(self.pop_size * GA.ELITISM)
+        for i in range(royalty):
+            pop_idx = self.sorted_fitness[i]
+            new_pop[i] = np.copy(self.pop[pop_idx])
+
+        # Have the most fit breed with the rest of the population
+        royal_kids = 0
+        while royal_kids < royalty:
+            a, b = self.CinderellaSelection(royalty)
+            c1, c2 = self.Crossover(a, b)
+            royal_kids += 2
+
+        # Fill the remainder of the population with random selection
+        new_pop_size = royalty + royal_kids
         while new_pop_size != self.pop_size:
             a, b = self.Selection()
-            self.Crossover(a, b)
-            new_pop_size += 1
+            c1, c2 = self.Crossover(a, b)
+            new_pop_size += 2
+
+        self.pop = new_pop
+
+    def CinderellaSelection(self, royalty):
+        """Select royalty and match with a peasant"""
+        royal = np.random.random_integers(low=0, high=royalty - 1)
+        peasant = np.random.random_integers(low=royalty, high=self.pop_size - 1)
+        return self.pop[royal], self.pop[peasant]
 
     def Selection(self):
         """Selects which two to perform crossover on"""
-        return None, None
+        lhs = np.random.random_integers(low=0, high=self.pop_size - 1)
+        rhs = lhs
+        while (rhs == lhs):
+            rhs = np.random.random_integers(low=0, high=self.pop_size - 1)
+        return self.pop[lhs], self.pop[rhs]
 
     def Crossover(self, a, b):
-        """Performs crossover on two selected individuals"""
-        pass
+        """Performs crossover on two selected individuals and returns the 
+           children to be added to new_pop"""
+        return None, None
+        c_a = a['center']['x'], a['center']['y']
+        c_b = b['center']
+        print(c_a)
+        # print(c_b)
+        # print(c_a + c_b)
+        exit()
+        center = np.array((a['center'] + b['center']), dtype=self.center)
+        print(center)
+        exit()
+        c1 = (a + b) / 2
+        print(c1)
+        exit()
 
 
 # 3
