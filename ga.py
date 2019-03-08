681539642b1bc6622d203d4d42a908001eb23085
Kyle MacMillan <macattackftw@gmail.com>
2019-03-07 19:54:52 -0700

    Attempted different breed strategy. Realized it may have been trying to fit the black region since that was the largest region...Added an image output per generation so I could see what it's trying to do.

--- ga.py
+++ ga.py
@@ -26,12 +26,19 @@ class GA():
             self.img_fitness = self.EvaluateImage()
             # print(self.img_fitness)
             self.InitializePop()    # 1
+            self.EvaluatePop()
+            self.Draw()
             for i in range(self.gens):
+                print('Best: ', self.pop[self.sorted_fitness[0]])
+                # print('Worst: ', self.fitness[self.sorted_fitness[self.pop_size - 1]])
+                # print('Fitness: ', self.fitness[self.sorted_fitness[0]])
+                if i < 10:
+                    self.Breed()
+                else:
+                    self.Breed2()        # 2
                 self.EvaluatePop()
-                # print('Best: ', self.pop[self.sorted_fitness[0]])
-                self.Breed()        # 2
-                # print('Generation: ', i)
-            self.EvaluatePop() 
+                self.Draw(self.UpdateImage(True))
+                print('Generation: ', i)
             self.UpdateImage()
             self.Draw()
             # print(self.pop)
@@ -44,7 +51,7 @@ class GA():
         self.EvaluatePop()
         self.UpdateImage()
         self.Draw()
-        sleep(900)
+        sleep(99999)
 
     def LoadImage(self, image):
         """Will load an image file into the format we need"""
@@ -80,6 +87,14 @@ class GA():
         for i in range(self.pop_size):
             temp = self.FillGenomes()
             self.pop[i] = self.FillGenomes()
+
+        # c = np.array((200, 200), dtype=self.center)
+        # c = np.array((75, 84), dtype=self.center)
+        # self.pop[0] = np.array((c, 48.0, 255), dtype=self.genome)
+        # self.Fitness2(self.pop[0])
+        # self.Draw()
+        # sleep(3)
+        # exit()
 
     def FillGenomes(self):
         """Generates a genome"""
@@ -125,37 +140,31 @@ class GA():
         # Note: individual is a numpy array of dtype=self.genome
 
         # https://stackoverflow.com/a/44874588/5492446
-
         center = individual['center']
 
         Y, X = np.ogrid[:self.height, :self.width]
         dist_from_center = np.sqrt((X - center['x'])**2 + (Y-center['y'])**2)
 
         mask = dist_from_center <= individual['radius']
-        circle_mask = mask * individual['intensity']
 
         # Where the magic begins
         result = np.where(mask)
 
         no_change = 0
         improvement = 0
+        # For each point in the mask
         for k in range(len(result[0])):
             i, j = result[0][k], result[1][k]
-            # print('i, j: {}, {}'.format(i, j))
-            # No change
-            # print(self.image[i, j])
-            # exit()
-            if self.art[i, j] == circle_mask[i, j]:
+            if self.art[i, j] == individual['intensity']:
                 no_change += 255
-            # print(self.image[i, j])
-            cm = np.int8(circle_mask[i, j])
+            cm = np.int32(individual['intensity'])
             improvement += 255 - np.abs(self.image[i, j] - cm)
 
         return improvement - no_change
 
     def SortPopulation(self):
         """Maps self.fitness to a sorted index list"""
-        self.sorted_fitness = np.argsort(self.fitness)
+        self.sorted_fitness = np.argsort(-self.fitness)
 
 
 # 2
@@ -194,6 +203,81 @@ class GA():
                 new_pop_size += 1
         
         self.pop = np.copy(new_pop)
+
+    def Breed2(self):
+        """Selection and Crossover to generate a new population"""
+        new_pop = np.zeros((self.pop_size, ), dtype=self.genome)
+
+        # Take the most fit and keep them
+        royalty = int(self.pop_size * GA.ELITISM)
+        for i in range(royalty):
+            pop_idx = self.sorted_fitness[i]
+            new_pop[i] = np.copy(self.pop[pop_idx])
+
+        # Have the most fit breed with the rest of the population
+        royal_kids = 0
+        while royal_kids < royalty:
+            a, b = self.CinderellaSelection(royalty)
+            c1, c2 = self.Crossover(a, b)
+            new_pop[royal_kids + royalty] = c1
+            royal_kids += 1
+            if royal_kids < royalty:
+                # Drop mutation if we're full
+                new_pop[royal_kids + royalty] = c2
+                royal_kids += 1
+
+        # Fill the remainder of the population with random selection
+        new_pop_size = royalty + royal_kids
+        while new_pop_size < self.pop_size - 6:
+            a, b = self.Selection()
+            c1, c2 = self.Crossover(a, b)
+            new_pop[new_pop_size] = c1
+            new_pop_size += 1
+            if new_pop_size < self.pop_size - 6:
+                # Drop mutation if we're full
+                new_pop[new_pop_size] = c2
+                new_pop_size += 1
+
+        best = self.BestMutation()
+        for i in range(6):
+            new_pop[new_pop_size + i] = best[i]
+        
+        self.pop = np.copy(new_pop)
+
+    def BestMutation(self):
+        pop_idx = self.sorted_fitness[0]
+        best = np.copy(self.pop[pop_idx])
+
+        # Mutate each attribute in both directions
+        rand_pos = np.random.normal(0.1, 0.1, 4)
+        rand_neg = np.random.normal(-0.1, 0.1, 4)
+        x = best['center']['x']
+        x1 = max(min((rand_pos[0] * x + x), self.width), 0)
+        x2 = max(min((rand_neg[0] * x + x), self.width), 0)
+        y = best['center']['y']
+        y1 = max(min((rand_pos[1] * y + y), self.height), 0)
+        y2 = max(min((rand_neg[1] * y + y), self.height), 0)
+
+        r = best['radius']
+        r1 = r * rand_pos[2] + r
+        r2 = r * rand_neg[2] + r
+
+        i = best['intensity']
+        i1 = i * rand_pos[3] + i
+        i2 = i * rand_neg[3] + i
+
+        c1 = np.array((x1, y1), dtype=self.center)
+        c2 = np.array((x2, y2), dtype=self.center)
+
+        # Mutations of the best
+        b1 = np.array((c1, best['radius'], best['intensity']), dtype=self.genome)
+        b2 = np.array((c2, best['radius'], best['intensity']), dtype=self.genome)
+        b3 = np.array((best['center'], r1, best['intensity']), dtype=self.genome)
+        b4 = np.array((best['center'], r2, best['intensity']), dtype=self.genome)
+        b5 = np.array((best['center'], best['radius'], i1), dtype=self.genome)
+        b6 = np.array((best['center'], best['radius'], i2), dtype=self.genome)
+
+        return b1, b2, b3, b4, b5, b6
 
     def CinderellaSelection(self, royalty):
         """Select royalty and match with a peasant"""
@@ -266,9 +350,10 @@ class GA():
 
 
 # 4 
-    def UpdateImage(self):
+    def UpdateImage(self, generation=False):
         """Update self.art with the most fit individual"""
         individual = self.pop[self.sorted_fitness[0]]
+        individual['intensity'] = min(individual['intensity'] * 2, 255)
         print(individual)
         center = individual['center']
 
@@ -277,9 +362,14 @@ class GA():
 
         mask = dist_from_center <= individual['radius']
         circle_value = mask * individual['intensity']
-        self.art = self.art + circle_value
+        if generation:
+            art = np.copy(self.art)
+            art += circle_value
+            return art
+        else: 
+            self.art = self.art + circle_value
 
-    def Draw(self):
+    def Draw(self, image=None):
         """Draw the output to the screen"""
         print('Drawing circle: ', self.epoch)
         if self.epoch == 0:
@@ -290,8 +380,12 @@ class GA():
             self.ax[1].axis('off')
             plt.ion()
         title = 'Circle: ' + str(self.epoch)
+        while len(plt.get_fignums()) > 1:
+            plt.close(fig=plt.get_fignums()[0])
         plt.show()
-        self.ax[0].imshow(self.art, cmap='gray', vmin = 0, vmax = 255)
+        if image is None:
+            image = self.art
+        self.ax[0].imshow(image, cmap='gray', vmin = 0, vmax = 255)
         self.ax[1].imshow(self.image, cmap='gray', vmin = 0, vmax = 255)
         self.ax[0].set_title(title)
         self.ax[1].set_title('Original Image')
