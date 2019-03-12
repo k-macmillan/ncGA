68b50c6b181d322053ceb5c1ac11ef37813041e3
Kyle MacMillan <macattackftw@gmail.com>
2019-03-11 23:37:58 -0600

    Revamped code to be faster. Added new images.

--- ga.py
+++ ga.py
@@ -2,90 +2,100 @@ import numpy as np
 import imageio
 import matplotlib.pyplot as plt
 from time import sleep, perf_counter
-# import cProfile
+import cProfile
 
 class GA():
     ELITISM = 0.10
-    def __init__(self, population=200, generations=200, circles=22):
+    def __init__(self, population=50, generations=50, circles=200, headless=False):
         self.pop_size = population
         self.gens = generations
         self.circles = circles
-        self.center = np.dtype([('x', np.uint16), ('y', np.uint16)])
-        self.genome = np.dtype([('center',self.center), ('radius', np.float64), ('intensity', np.int16) ])
+        self.center = np.dtype([('x', np.float32), ('y', np.float32)])
+        self.genome = np.dtype([('center',self.center), ('radius', np.float32), ('intensity', np.float32) ])
         self.pop = np.zeros((self.pop_size, ), dtype=self.genome)
+        self.headless = headless
 
     def Reset(self):
         self.img_fitness = 0
 
-    def Run(self, image, circles=None):
+    def Run(self, image):
         """Run the Genetic Algorthm"""
-        if circles is not None:
-            self.circles = circles
         filename = image[7:len(image) - 4]
         self.LoadImage(image)
         self.Reset()
-        print('Running...')
         self.epoch = 0
-        while self.epoch != self.circles:
+        while self.epoch < self.circles:
             self.InitializePop()    # 1
             self.EvaluatePop()
-            self.Draw()
+            if not self.headless:
+                self.Draw()
+
             for self.gen in range(self.gens):
-                # print('Generation: ', self.gen)
-                # print('Best: ', self.pop[self.sorted_fitness[0]])
-                # print('Worst:   ', self.fitness[self.sorted_fitness[self.pop_size - 1]])
-                # print('Fitness: ', self.fitness[self.sorted_fitness[0]])
                 self.Breed()        # 2
                 self.EvaluatePop()
             self.UpdateImage()
-            # print(self.pop)
-            # print()
-            # print(self.fitness)
-            # print()
-            # print(self.sorted_fitness)
-            # sleep(7)
-            # exit()
+
+            # For each epoch draw only if headless. Print on 50/100/200
+            if self.epoch == 0:
+                if self.headless:
+                    self.Draw()
+            elif self.epoch == 49:
+                if self.headless:
+                    self.Draw()
+                plt.savefig('results/' + filename + '_50.png')
+            elif self.epoch == 99:
+                if self.headless:
+                    self.Draw()
+                plt.savefig('results/' + filename + '_100.png')
+            elif self.epoch == 199:
+                if self.headless:
+                    self.Draw()
+                plt.savefig('results/' + filename + '_200.png')
             self.epoch += 1
-        self.EvaluatePop()
-        self.UpdateImage()
-        self.Draw()
-        plt.savefig('results/' + filename + '_' + str(self.circles) + '_v2.png')
-        # sleep(15)
+
+        # When done display final image
+        if not self.headless:
+            self.epoch -= 1
+            self.Draw()
+            sleep(10)
+
 
     def LoadImage(self, image):
         """Will load an image file into the format we need"""
         # self.image = np.invert(imageio.imread(image))
-        self.image = imageio.imread(image)
+        self.image = np.array(imageio.imread(image), dtype=np.float32)
         # Account for images with 3 dimensions
         if len(self.image.shape) == 3:
             self.height, self.width, _ = self.image.shape
             # Apply grayscale...
         else:
             self.height, self.width = self.image.shape
-        self.max_dim = max(self.height, self.width)
+        self.max_dim = np.float32(max(self.height, self.width))
         self.perfect_image = np.sum(self.image)
-        self.max_image = 255 * self.width * self.height
-        self.art = np.zeros((self.height, self.width))
-        self.max_radius = self.max_dim
+        # self.max_image = 255 * self.width * self.height
+        self.art = np.zeros((self.height, self.width), dtype=np.float32)
+        self.max_radius = self.max_dim / 2.0
+        self.pixel_modifier = np.float32(0.00001)
 
 # 1
     def InitializePop(self):
         """Initialize pop"""
         # self.pop = np.fromiter((self.FillGenomes() for _ in self.pop), self.genome)
-        self.rand_x = np.random.random_integers(low=0,
-                                                high=self.width - 1,
-                                                size=self.pop_size)
-        self.rand_y = np.random.random_integers(low=0,
-                                                high=self.height - 1,
-                                                size=self.pop_size)
-        self.rand_r = np.random.uniform(low=1.0,
-                                        high=self.max_radius / 2.0,
+        self.rand_x = np.random.uniform(low=0,
+                                        high=self.width - 1,
                                         size=self.pop_size)
-        self.rand_i = np.random.random_integers(low=-255,
-                                                high=255,
-                                                size=self.pop_size)
+        self.rand_y = np.random.uniform(low=0,
+                                        high=self.height - 1,
+                                        size=self.pop_size)
+        self.rand_r = np.random.uniform(low=2.0,
+                                        high=self.max_radius,
+                                        size=self.pop_size)
+        self.rand_i = np.random.uniform(low=-255.0,
+                                        high=255.0,
+                                        size=self.pop_size)
         for i in range(self.pop_size):
             self.pop[i] = self.FillGenomes(i)
+        self.fitness = np.zeros(self.pop_size, dtype=np.float32)
 
     def FillGenomes(self, i):
         """Generates a genome"""
@@ -95,7 +105,6 @@ class GA():
 
     def EvaluatePop(self):
         """Evaluates each individual and sorts them"""
-        self.fitness = np.zeros(self.pop_size)
         for i in range(self.pop_size):
             self.fitness[i] = self.Fitness(self.pop[i])
         # Sort
@@ -108,15 +117,15 @@ class GA():
         # https://stackoverflow.com/a/44874588/5492446
         center = individual['center']
         Y, X = np.ogrid[:self.height, :self.width]
-        dist_from_center = np.sqrt((X - center['x'])**2 + (Y-center['y'])**2)
+        dist_from_center = np.sqrt((X - center['x'])**2 + (Y-center['y'])**2, dtype=np.float32)
 
         mask = dist_from_center <= individual['radius']
-
         # Where the magic begins
-        pixel_count = np.sum(mask)
+        pixel_count = np.sum(mask, dtype=np.float32)
         circle = mask * individual['intensity']
         art = self.art + circle
-        return np.sum(np.abs(self.image - art))  - pixel_count * 0.00001
+
+        return np.sum(np.abs(self.image - art)) - pixel_count * self.pixel_modifier
 
 # 2
     def Breed(self):
@@ -181,12 +190,12 @@ class GA():
         y2 = max(min((rand_neg[1] * y + y), self.height), 0)
 
         r = best['radius']
-        r1 = r * rand_pos[2] + r
-        r2 = r * rand_neg[2] + r
+        r1 = max(r * rand_pos[2] + r, 2.0)
+        r2 = max(r * rand_neg[2] + r, 2.0)
 
         i = best['intensity']
-        i1 = min(i * rand_pos[3] + i, 255)
-        i2 = max(i * rand_neg[3] + i, -255)
+        i1 = min(i * rand_pos[3] + i, 255.0)
+        i2 = max(i * rand_neg[3] + i, -255.0)
 
         c1 = np.array((x1, y1), dtype=self.center)
         c2 = np.array((x2, y2), dtype=self.center)
@@ -225,8 +234,8 @@ class GA():
 
     def AverageGeneome(self, a, b):
         """Averages two genomes"""
-        x = (a['center']['x'] + b['center']['x']) // 2
-        y = (a['center']['y'] + b['center']['y']) // 2
+        x = (a['center']['x'] + b['center']['x']) / 2.0
+        y = (a['center']['y'] + b['center']['y']) / 2.0
 
         center = np.array((x, y), dtype=self.center)
         radius = (a['radius'] + b['radius']) / 2.0
@@ -234,54 +243,34 @@ class GA():
         intensity += a['intensity']
         intensity += b['intensity']
 
-        intensity = intensity // 2
+        intensity = intensity / 2.0
         return np.array((center, radius, intensity), dtype=self.genome)
 
 # 3
     def MutateGeneome(self, individual):
         """Mutatation applied to a genome"""
-        # NOTE: Test out normal and uniform. Uniform is robus, normal is for 
-        #       fine-tuning. Possibly allow generation to determine.
-        uniform = False
-        if uniform:
-            x = individual['center']['x']
-            x = max(min((np.random.uniform(low=-1.0, high=1.0) * x + x), self.width), 0)
-            y = individual['center']['y']
-            y = max(min((np.random.uniform(low=-1.0, high=1.0) * y + y), self.height), 0)
-            
-            center = np.array((x, y), dtype=self.center)
-            rand = np.random.uniform(low=-1.0, high=1.0)
-            radius = max(rand * individual['radius'] + individual['radius'], 1.0)
-            rand = np.random.uniform(low=-1.0, high=1.0)
-            intensity = max(rand * individual['intensity'] + individual['intensity'], 1.0)
-            
-            return np.array((center, radius, intensity), dtype=self.genome)
+        if self.gen < 3:
+            rand = np.random.normal(0.0, 0.3, 4)
+        elif self.gen < self.gens / 2.0:
+            rand = np.random.normal(0.0, 0.2, 4)
         else:
-            # Normal distribution
-            if self.gen < 3:
-                rand = np.random.normal(0.0, 0.3, 4)
-            elif self.gen < self.gens / 2.0:
-                rand = np.random.normal(0.0, 0.2, 4)
-            else:
-                rand = np.random.normal(0.0, 0.1, 4)
-            x = individual['center']['x']
-            x = max(min((rand[0] * x + x), self.width), 0)
-            y = individual['center']['y']
-            y = max(min((rand[1] * y + y), self.height), 0)
-            
-            center = np.array((x, y), dtype=self.center)
-            radius = max(rand[2] * individual['radius'] + individual['radius'], 1.0)
-            intensity = min(rand[3] * individual['intensity'] + individual['intensity'], 255)
-            
-            return np.array((center, radius, intensity), dtype=self.genome)
+            rand = np.random.normal(0.0, 0.1, 4)
+        x = individual['center']['x']
+        x = max(min((rand[0] * x + x), self.width), 0)
+        y = individual['center']['y']
+        y = max(min((rand[1] * y + y), self.height), 0)
+        
+        center = np.array((x, y), dtype=self.center)
+        radius = max(rand[2] * individual['radius'] + individual['radius'], 2.0)
+        intensity = min(rand[3] * individual['intensity'] + individual['intensity'], 255)
+        
+        return np.array((center, radius, intensity), dtype=self.genome)
 
 
 # 4 
     def UpdateImage(self):
         """Update self.art with the most fit individual"""
         individual = self.pop[self.sorted_fitness[0]]
-        # print('individual: ', individual)
-        # print('fitness:    ', self.fitness[self.sorted_fitness[0]])
         center = individual['center']
 
         Y, X = np.ogrid[:self.height, :self.width]
@@ -291,9 +280,10 @@ class GA():
         circle_value = mask * individual['intensity']
         self.art = self.art + circle_value
 
-    def Draw(self, image=None):
+    def Draw(self):
         """Draw the output to the screen"""
-        print('Drawing circle: ', self.epoch)
+        if not self.headless:
+            print('Drawing circle: ', self.epoch + 1)
         if self.epoch == 0:
             plt.clf()
             fig, self.ax = plt.subplots(1, 2)
@@ -301,25 +291,22 @@ class GA():
             self.ax[0].axis('off')
             self.ax[1].axis('off')
             plt.ion()
-        title = 'Circle: ' + str(self.epoch)
-        while len(plt.get_fignums()) > 1:
-            plt.close(fig=plt.get_fignums()[0])
-        plt.show()
-        if image is None:
-            image = self.art
-        self.ax[0].imshow(image, cmap='gray', vmin = 0, vmax = 255)
+        title = 'Circle: ' + str(self.epoch + 1)
+
+        if not self.headless:
+            plt.show()
+        self.ax[0].imshow(self.art, cmap='gray', vmin = 0, vmax = 255)
         self.ax[1].imshow(self.image, cmap='gray', vmin = 0, vmax = 255)
         self.ax[0].set_title(title)
         self.ax[1].set_title('Original Image')
-        plt.pause(.001)
+        if not self.headless:
+            plt.pause(.001)
 
 
 
 if __name__ == '__main__':
-    ga = GA()
-    # cProfile.run('ga.Run("images/k.png")', sort="time")
-    ga.Run('images/darwin.png', circles=5)
-    ga.Run('images/darwin.png', circles=100)
-    ga.Run('images/darwin.png', circles=200)
+    ga = GA(headless=False)
+    # cProfile.run('ga.Run("images/test3.png")', sort="time")
+    ga.Run('images/davinci.png')
     plt.ioff()
 
